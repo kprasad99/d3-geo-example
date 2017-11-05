85927dae2f0a01e1d633380c2ef8298dfb039853
Karthik Prasad <adkp.mlr@gmail.com>
2017-11-05 18:42:42 +0530

    Implementation of V3 version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # d3-geo-example
 An example project for d3 geo both v3 and v4
+
+### RUN
+
+```
+mvn spring-boot:run
+
+```
+
+### V3 version url
+
+```
+http://localhost:8080/d3-geo.v3.html
+
+```
+
---
+++ d3-geo-example/src/main/java/com/kp/swasthik/d3/D3GeoExampleApplication.java
@@ -0,0 +1,12 @@
+package com.kp.swasthik.d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3GeoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D3GeoExampleApplication.class, args);
+	}
+}
